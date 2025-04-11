998e84a02ec021d37cfbff23f373c07f072a7904
KrazyMiner001 <krazyminer001@outlook.com>
2025-04-11 16:32:01 +0200

    Initial commit

---
+++ src/client/java/krazyminer001/playtime/ServerPlaytimeManagerClient.java
@@ -0,0 +1,10 @@
+package krazyminer001.playtime;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ServerPlaytimeManagerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/krazyminer001/playtime/ServerPlaytimeManagerDataGenerator.java
@@ -0,0 +1,11 @@
+package krazyminer001.playtime;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ServerPlaytimeManagerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
