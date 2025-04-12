fe64cf01b87ece652edcddb2c2d4f4bedffd58da
KrazyMiner001 <krazyminer001@outlook.com>
2025-04-12 11:34:00 +0200

    feat(client): Added translations

--- src/client/java/krazyminer001/playtime/ServerPlaytimeManagerDataGenerator.java
+++ src/client/java/krazyminer001/playtime/ServerPlaytimeManagerDataGenerator.java
@@ -1,11 +1,14 @@
 package krazyminer001.playtime;
 
+import krazyminer001.playtime.datagen.lang.EnUsLangProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class ServerPlaytimeManagerDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+		pack.addProvider(EnUsLangProvider::new);
 	}
 }
---
+++ src/client/java/krazyminer001/playtime/datagen/lang/EnUsLangProvider.java
@@ -0,0 +1,18 @@
+package krazyminer001.playtime.datagen.lang;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class EnUsLangProvider extends FabricLanguageProvider {
+    public EnUsLangProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, "en_us", registryLookup);
+    }
+
+    @Override
+    public void generateTranslations(RegistryWrapper.WrapperLookup wrapperLookup, TranslationBuilder translationBuilder) {
+        translationBuilder.add("playtime.disconnect.overtime", "You have exceeded your max daily playtime today of %s ticks, the next window where time is not tracked is %s");
+    }
+}
